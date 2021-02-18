ff9be7a35f32580b7577fdae8b9bab38bd079fc1
MariamPoghosyan8 <pogosyanmariam8@gmail.com>
2021-02-18 17:41:02 +0400

    add typescript & Eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index-bundle.js"
+    path: path.join(__dirname, '/dist'),
+    filename: 'index-bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: "/node_modules/",
-        use: ["babel-loader"]
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules/',
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '*'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
+      template: './public/index.html',
+    }),
+  ],
 };
